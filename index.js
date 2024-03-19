ad1bfc7e1d51e281737e804cb7e7021bdea11952
pascal <pascal.nicintije@student.moringaschool.com>
2024-03-19 15:06:36 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+saturdayFun("bathe my dogs")
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+mondayWork("work from home")
+function wrapAdjective(wrapper ="*") {
+    return function inner(adjective="special") {
+        return `You are ${wrapper}${adjective}${wrapper}!`
+    }
+}
